2fdd4f9e95e23a69dd2500dc0ad872f88853158a
jammoo <dinoman0624@hotmail.com>
2020-07-19 23:05:55 -0400

    Added functionality to button, added images for dice, added new favicon

--- RollDice-Angular/src/app/content/content.component.ts
+++ RollDice-Angular/src/app/content/content.component.ts
@@ -8,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class ContentComponent implements OnInit {
 
   constructor() { }
-
+  num = Math.floor((Math.random() * 5) + 1);
+  sideName = "assets/images/dice/side_" + this.num + ".png";
+  newNum();
+  newNum() {
+    this.num = Math.floor((Math.random() * 5) + 1);
+    this.sideName = "assets/images/dice/side_" + this.num + ".png";
+  };
   ngOnInit(): void {
   }
 
