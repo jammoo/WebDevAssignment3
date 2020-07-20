7d61ed20c1aa083ddd8ffb9c748af92b53828ad3
jammoo <dinoman0624@hotmail.com>
2020-07-19 22:19:48 -0400

    Added basic text and styling

--- RollDice-Angular/src/app/footer/footer.component.ts
+++ RollDice-Angular/src/app/footer/footer.component.ts
@@ -8,6 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   constructor() { }
+  sname = "James Bhagoutie";
+  sid = 991554085;
+  date = new Date();
+  year = this.date.getFullYear();
+  months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+  month = this.months[this.date.getMonth()];
+  day = this.date.getDate();
+  //footer = "<i>John Smith student Id 999992342 </i><b>Sheridan College&nbsp;&nbsp;<span class='hart'>&#9825;</span>&nbsp;&nbsp;";
+    //$("body").append("<footer></footer>");
+    //$("footer")
+    //    .html("<i>John Smith student Id 999992342 </i><b>Sheridan College&nbsp;&nbsp;<span class='hart'>&#9825;</span>&nbsp;&nbsp;")
+    //    .append(`${month} ${day}, ${year}`);
 
   ngOnInit(): void {
   }
--- RollDice-Angular/src/app/header/header.component.ts
+++ RollDice-Angular/src/app/header/header.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
-
+  sname = "James Bhagoutie";
+  sid = 991554085;
   ngOnInit(): void {
   }
 
